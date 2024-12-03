a52f2c8d6e0adc6ef6b84bde0e14093b9a32bcfe
SooYeon Seong <soolotus632@gmail.com>
2024-12-03 08:37:08 -0700

    add uploading picture function

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import streamlit as st
 from PIL import  Image
 
+st.subheader("Color to Grayscale Converter")
+uploaded_image = st.file_uploader("Upload Image")
+
 with st.expander("Start Camera"):
     camera_image= st.camera_input('Camera')
 
 if camera_image:
     img = Image.open(camera_image)
-
     grey_img = img.convert("L")
-
     st.image(grey_img)
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    grey_img = img.convert("L")
+    st.image(grey_img)
+
+
